ac98f3ef1722e3f01f0f5f9f949ea510781cd668
ling910520 <ling910520@gmail.com>
2019-11-08 19:37:07 +0800

    table rendered

--- pages/tool/[id].js
+++ pages/tool/[id].js
@@ -1,31 +1,37 @@
 import fetch from 'isomorphic-unfetch';
+import orderBy from "lodash/orderBy";
 
 
 const tool = (props) =>{
-    let header = Object.keys(props.toolTableData[0])
+    let header = Object.keys(props.toolTableData[0]).filter(x=>(x!=='RF_HRS')) // take out rf_hrs columns
+    let body = orderBy(props.toolTableData,'reporting_date','desc') //order desc by reporting date
     return (
-        <div className="table-container">
-            <table id='students' className="table is-bordered is-hoverable">
-                <thead>
+        <div className="section table-container">
+            <div className="columns is-pulled-left">
+            <table className="table is-bordered is-hoverable">
+                <thead className="">
                     <tr >
                     {header.map((key,index)=>(
-                        <th key={key}>{key}</th>
-                    )
-                    )}
+                                <th key={index} className='is-info'>{key}</th>
+                    ))}
+    
                     </tr>
                 </thead>
               <tbody>
-              {props.toolTableData.map(data =>(
-                  <tr>
-                            <td>{data.reporting_date}</td>
-                            <td>{data.RF_HRS}</td>
-                            <td>{data.PlatenRFHours}</td>
-                            </tr>
-              ))}
+              {
+                body.map((key,index)=>(
+                    <tr key={index}>
+                            <td>{key.reporting_date}</td>
+                            <td>{key.PlatenRFHours}</td>
+                    </tr>
+                ))
+              }
+
               </tbody>
             </table>
             {/* <CSVLink data={this.state.students}>Download me</CSVLink>
                         <CSVDownload data={this.state.students} target="_blank" /> */}
+        </div>
         </div>
     )
 }
@@ -34,7 +40,6 @@ tool.getInitialProps  = async function(context){
     const { id } = context.query;
     const res = await fetch(`http://sgpatsprod01:5000/tooldata/${id}`);
     const toolTableData = await res.json();
-    
     return {toolTableData};
 
 }
