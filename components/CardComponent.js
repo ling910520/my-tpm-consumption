89757e617ec433c249377fc5cbf27317f75619c6
ling910520 <ling910520@gmail.com>
2019-11-11 16:43:25 +0800

    sort data

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,5 +1,14 @@
-import Layout from '../components/Layout'
+import React, { useState,useEffect } from 'react';
+
+
 const CardComponent = () =>{
+    const data = [{"reporting_date":"2019-11-08","RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":74.46933746},
+    {"reporting_date":"2019-11-09","RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":85.63039398},
+    {"reporting_date":"2019-11-10","RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":92.68686676},
+    {"reporting_date":"2019-11-11","RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247}]
+const [Time, setTime] = useState(new Date());
+
+
 return(
     <div classNameName="container notification">
         <div classNameName="columns">
@@ -22,7 +31,7 @@ return(
                             <li> Metal 2 :</li>
                         </ul>
                         <br />
-                        <time datetime="2016-1-1"><span>Last updated: 11:09 PM - 1 Jan 2016</span></time>
+                        <time datetime="2016-1-1"><span>Last updated: {Time.toDateString()}</span></time>
                     </div>
                 </div>
                 {/* <footer className="card-footer">
@@ -40,3 +49,15 @@ return(
 
 
 export default CardComponent;
+
+
+// SideMenu.getInitialProps = async function() {
+//     const res = await fetch('http://sgpatsprod01:8080/tpmtestdata');
+//     const data = await res.json();
+  
+//     console.log(`Show data fetched. Count: ${data.length}`);
+  
+//     return {
+//       data
+//     };
+//   }
--- pages/table.js
+++ pages/table.js
@@ -104,3 +104,5 @@ return(
 
 
 export default Table;
+
+
