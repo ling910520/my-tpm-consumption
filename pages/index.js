de48c68a430d354ee88ab8af8a82a4a5e1a6cd9c
unknown <you@example.com>
2019-11-12 23:14:11 +0800

    reducer filtered

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Layout from '../components/Layout'
 import Kanban from '../components/kanban'
 import '../styles/styles.css'
+import {returnedFromTool} from '../data/data'
+import orderBy from "lodash/orderBy";
 
 const Home = () =>{
-const data = [
-{"reporting_date":"2019-11-11",'eqp_id':'3SPT-20',"cathode1":113.734076,"cathode3":248.964495},  
-{"reporting_date":"2019-11-11",'eqp_id':'3DE-02',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247}]
+const dataFromTool = [
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-20',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-21',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-22',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-23',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-24',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3DE-03',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247,limit:100},
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-25',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-26',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-27',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+{"reporting_date":"2019-11-11",'eqp_id':'3SPT-28',"cathode1":113.734076,"cathode3":248.964495,limit:100},  
+
+{"reporting_date":"2019-11-11",'eqp_id':'3DE-02',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247,limit:100}]
+let data = orderBy(dataFromTool,['reporting_date','eqp_id']) //order desc by reporting date
 
 // let reporting_date = Object.values(data)[0] // take only latest reporting_date
 //  let latestdate =  new Date()
