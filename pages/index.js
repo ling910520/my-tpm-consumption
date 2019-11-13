88ce38c0e432a8697cfc89364561a8c68401a51a
ling910520 <ling910520@gmail.com>
2019-11-13 08:36:51 +0800

    bk

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import '../styles/styles.css'
 const Home = () =>{
 const data = [
 {"reporting_date":"2019-11-11",'eqp_id':'3SPT-20',"cathode1":113.734076,"cathode3":248.964495},  
-{"reporting_date":"2019-11-11",'eqp_id':'3DE-02',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247}]
+{"reporting_date":"2019-11-11",'eqp_id':'3DE-02',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247},
+{"reporting_date":"2019-11-11",'eqp_id':'3DE-03',"RF_HRS":"Stat3_Etch_MV_PlatenRFHours","PlatenRFHours":94.50147247}]
 
 // let reporting_date = Object.values(data)[0] // take only latest reporting_date
 //  let latestdate =  new Date()
