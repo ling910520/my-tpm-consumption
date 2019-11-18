ede4dbf294318c91bcc6486257521cc966ea0b3c
unknown <you@example.com>
2019-11-18 22:05:19 +0800

    async function

--- pages/success.js
+++ pages/success.js
@@ -4,10 +4,24 @@ import fetch from 'isomorphic-unfetch';
 
 const success = () =>{
 
+    async function buttonClick() {
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=batman`);
+        const json = await response.json();
+        console.log(response.status)
+        console.log(json)
 
+      }
+    const alertme = event=>{
+        event.preventDefault()
+        alert('alertme')
+        alert(`${window.location.host}`)
+    }
+  
 return(
     <Layout>
-            <h1>Success</h1>
+           <button onClick={buttonClick}>Click</button>
+           <button onClick={alertme}>alert</button>
+
     </Layout>
 )
 }
