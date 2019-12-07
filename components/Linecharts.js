eba29b29b445ef3b1f4a14585505a012482327d2
unknown <you@example.com>
2019-12-07 22:54:50 +0800

    series to fix spt lines

--- components/Linecharts.js
+++ components/Linecharts.js
@@ -63,7 +63,14 @@ const Linecharts = (props) => {
             const Cu_Target = data.filter(row => row.svid_name === 'Cu Target')
             const Etch_Shield = data.filter(row => row.svid_name === 'Etch Shield')
             const Etch_Quartz = data.filter(row => row.svid_name === 'Etch Quartz Shield')
-
+            
+            const series = [{name: 'Ti_Shield', data:Ti_Shield},
+            {name: 'Ti_Target', data:Ti_Target},
+            {name: 'Cu_Shield', data:Cu_Shield},
+            {name: 'Cu_Target', data:Cu_Target},
+            {name: 'Etch_Shield', data:Etch_Shield},
+            {name: 'Etch_Quartz', data:Etch_Quartz},
+            ]
 
             return (
                 <div>
@@ -90,28 +97,28 @@ const Linecharts = (props) => {
                                 height={scale * 100}
                             >
                                 <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="inserted_timestamp" tick={false} />
+                                <XAxis dataKey="inserted_timestamp" tick={false} type="category" allowDuplicatedCategory={false} />
                                 <YAxis interval="preserveStartEnd" />
                                 <Tooltip />
                                 <Legend />
 
                                 {Ti_Target.length>0?<ReferenceLine name="Ti Target" y={parseInt(Ti_Target[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Ti Target (${parseInt(Ti_Target[0]['consumption_limit'])})`}  />:''}
-                                {Ti_Target.length>0?<Line dataKey="svid_value" data={Ti_Target} name="Ti_Target" stroke="blue" key="Ti_Target" />:''}
+                                {Ti_Target.length>0?<Line dataKey="svid_value" data={series[1]['data']} name="Ti_Target" stroke="blue" key="Ti_Target" />:''}
 
                                 {Ti_Shield.length>0?<ReferenceLine name="Ti Shield" y={parseInt(Ti_Shield[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Ti Shield ${parseInt(Ti_Shield[0]['consumption_limit'])}`}  />:''}
-                                {Ti_Shield.length>0?<Line dataKey="svid_value" data={Ti_Shield} name="Ti_Shield" stroke="pink" key="Ti_Shield" />:''}
+                                {Ti_Shield.length>0?<Line dataKey="svid_value" data={series[0]['data']} name="Ti_Shield" stroke="pink" key="Ti_Shield" />:''}
 
                                 {Cu_Target.length>0?<ReferenceLine name="Cu Target" y={parseInt(Cu_Target[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Cu Target (${parseInt(Cu_Target[0]['consumption_limit'])})`}  />:''}
-                                {Cu_Target.length>0?<Line dataKey="svid_value" data={Cu_Target} name="Cu_Target" stroke="orange" key="Cu_Target" />:''}
+                                {Cu_Target.length>0?<Line dataKey="svid_value" data={series[3]['data']} name="Cu_Target" stroke="orange" key="Cu_Target" />:''}
 
                                 {Cu_Shield.length>0?<ReferenceLine name="Cu Shield" y={parseInt(Cu_Shield[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Cu Shield ${parseInt(Cu_Shield[0]['consumption_limit'])}`}  />:''}
-                                {Cu_Shield.length>0?<Line dataKey="svid_value" data={Cu_Shield} name="Cu_Shield" stroke="yellow" key="Cu_Shield" />:''}
+                                {Cu_Shield.length>0?<Line dataKey="svid_value" data={series[2]['data']} name="Cu_Shield" stroke="yellow" key="Cu_Shield" />:''}
 
                                 {Etch_Shield.length>0?<ReferenceLine name="Etch_Shield" y={parseInt(Etch_Shield[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Etch_Shield (${parseInt(Etch_Shield[0]['consumption_limit'])})`}  />:''}
-                                {Etch_Shield.length>0?<Line dataKey="svid_value" data={Etch_Shield} name="Etch_Shield" stroke="green" key="Etch_Shield" />:''}
+                                {Etch_Shield.length>0?<Line dataKey="svid_value" data={series[4]['data']} name="Etch_Shield" stroke="green" key="Etch_Shield" />:''}
                                
                                 {Etch_Quartz.length>0?<ReferenceLine name="Etch_Quartz" y={parseInt(Etch_Quartz[0]['consumption_limit'])} stroke="red" ifOverflow="extendDomain" label={`Etch_Quartz (${parseInt(Etch_Quartz[0]['consumption_limit'])})`}  />:''}
-                                {Etch_Quartz.length>0?<Line dataKey="svid_value" data={Etch_Quartz} name="Etch_Quartz" stroke="grey" key="Etch_Quartz" />:''}
+                                {Etch_Quartz.length>0?<Line dataKey="svid_value" data={series[5]['data']} name="Etch_Quartz" stroke="grey" key="Etch_Quartz" />:''}
 
                             </LineChart>
                         </div>
